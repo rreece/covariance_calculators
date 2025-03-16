da5ffde92737b134cd1129a3a50283f73258ab8b
Ryan Reece <ryan.d.reece@live.com>
2025-03-15 17:09:25 -0700

    update

--- estimators.py
+++ estimators.py
@@ -16,6 +16,9 @@ import math
 import numpy as np
 
 
+DEFAULT_DTYPE = np.float64
+
+
 def calc_sample_mean(data, frequency=None):
     _mean = np.mean(data, axis=0)
     if not frequency is None:
@@ -35,10 +38,12 @@ class OnlineCovariance:
     A class to calculate the mean and the covariance matrix
     of the incrementally added, n-dimensional data.
 
+    Uses the Welford (1962) one-pass algorithm.
+
     Adapted from:
     https://carstenschelp.github.io/2019/05/12/Online_Covariance_Algorithm_002.html
     """
-    def __init__(self, order, frequency=1):
+    def __init__(self, order, frequency=1, dtype=DEFAULT_DTYPE):
         """
         Parameters
         ----------
@@ -47,9 +52,10 @@ class OnlineCovariance:
         """
         self._order = order
         self._count = 0
-        self._mean = np.zeros(order)
-        self._Cn = np.zeros((order, order))
+        self._mean = np.zeros(order, dtype=dtype)
+        self._Cn = np.zeros((order, order), dtype=dtype)
         self.frequency = frequency
+        self.dtype = dtype
         
     @property
     def count(self):
@@ -98,25 +104,25 @@ class OnlineCovariance:
         observation: array_like, The observation to add.
         """
         if isinstance(observation, np.ndarray):
-            _obs = observation
+            obs = observation
         elif isinstance(observation, list):
-            _obs = np.array(observation, dtype=float)
+            obs = np.array(observation, dtype=self.dtype)
         elif isinstance(observation, int) or isinstance(observation, float):
-            _obs = np.array([observation], dtype=float)
+            obs = np.array([observation], dtype=self.dtype)
         else:
             assert False
 
-        if self._order != _obs.size:
-            raise ValueError(f"To add, observation size {_obs.size} must be {self._order}")
+        if self._order != obs.size:
+            raise ValueError(f"To add, observation size {obs.size} must be {self._order}")
 
         self._count += 1
         if self.count == 1: # First entry 
-            self._mean = np.array(_obs, dtype=float)
+            self._mean = np.array(obs, dtype=self.dtype)
             return
 
-        delta = _obs - self._mean
+        delta = obs - self._mean
         self._mean += delta / self.count
-        delta_at_n = _obs - self._mean
+        delta_at_n = obs - self._mean
 
         self._Cn += np.outer(delta, delta_at_n)
 
@@ -151,8 +157,8 @@ class OnlineCovariance:
 
 
 class EMACovariance(OnlineCovariance):
-    def __init__(self, order, alpha=None, halflife=None, span=None, frequency=1):
-        super(EMACovariance, self).__init__(order, frequency=frequency)
+    def __init__(self, order, alpha=None, halflife=None, span=None, frequency=1, dtype=DEFAULT_DTYPE):
+        super(EMACovariance, self).__init__(order, frequency=frequency, dtype=dtype)
         _alpha = self._calc_alpha(alpha=alpha, halflife=halflife, span=span)
         assert 0 < _alpha < 1
         self._alpha = _alpha
@@ -176,25 +182,25 @@ class EMACovariance(OnlineCovariance):
 
     def add(self, observation):
         if isinstance(observation, np.ndarray):
-            _obs = observation
+            obs = observation
         elif isinstance(observation, list):
-            _obs = np.array(observation, dtype=float)
+            obs = np.array(observation, dtype=self.dtype)
         elif isinstance(observation, int) or isinstance(observation, float):
-            _obs = np.array([observation], dtype=float)
+            obs = np.array([observation], dtype=self.dtype)
         else:
             assert False
 
-        if self._order != _obs.size:
-            raise ValueError(f"To add, observation size {_obs.size} must be {self._order}")
+        if self._order != obs.size:
+            raise ValueError(f"To add, observation size {obs.size} must be {self._order}")
 
         self._count += 1
         if self.count == 1: # First entry 
-            self._mean = np.array(_obs, dtype=float)
+            self._mean = np.array(obs, dtype=self.dtype)
             return
 
-        delta = _obs - self._mean
-        self._mean = (1 - self._alpha) * self._mean + self._alpha * _obs
-        delta_at_n = _obs - self._mean
+        delta = obs - self._mean
+        self._mean = (1 - self._alpha) * self._mean + self._alpha * obs
+        delta_at_n = obs - self._mean
 
         if self.count == 2: # Second entry 
             self._Cn = np.outer(delta, delta_at_n)
@@ -207,4 +213,61 @@ class EMACovariance(OnlineCovariance):
         TODO: Update for EMACovariance
         """
         assert False, "Not implemented; Probably won't."
+
+
+class RollingCovariance(OnlineCovariance):
+    """
+    Uses the Welford (1962) one-pass algorithm, also in reverse to
+    remove observations that leave the rolling window.
+
+    See:
+    https://stackoverflow.com/questions/30876298/removing-a-prior-sample-while-using-welfords-method-for-computing-single-pass-v
+    
+    """
+    def __init__(self, order, span=None, frequency=1, dtype=DEFAULT_DTYPE):
+        super(RollingCovariance, self).__init__(order, frequency=frequency, dtype=dtype)
+        self._span = span
+        self.queue = list()
+
+    def add(self, observation):
+        """
+        TODO: test me
+        """
+        if isinstance(observation, np.ndarray):
+            obs = observation
+        elif isinstance(observation, list):
+            obs = np.array(observation, dtype=self.dtype)
+        elif isinstance(observation, int) or isinstance(observation, float):
+            obs = np.array([observation], dtype=self.dtype)
+        else:
+            assert False
+
+        if self._order != obs.size:
+            raise ValueError(f"To add, observation size {obs.size} must be {self._order}")
+
+        if self.count == self._span:
+            assert len(self.queue) == self.count
+
+            # pop oldest
+            obs_old = self.queue.pop(0)
+            assert len(self.queue) == self._span - 1
+
+            delta_old_at_n = obs_old - self.mean
+            self._mean -= (obs_old - self.mean)/(self.count - 1)
+            delta_old = obs_old - self.mean
+            self._Cn -= np.outer(delta_old, delta_old_at_n)
+
+        self.queue.append(obs)
+        self._count = len(self.queue)
+        assert 1 <= self._count <= self._span
+
+        if self.count == 1: # First entry 
+            self._mean = np.array(obs, dtype=self.dtype)
+            return
+
+        delta = obs - self._mean
+        self._mean += delta / self.count
+        delta_at_n = obs - self._mean
+
+        self._Cn += np.outer(delta, delta_at_n)
 
